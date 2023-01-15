a41fc640e317ab77998541f4d338d3f4079a40f4
Maksymilian <maksymilianwojnowski@gmail.com>
2023-01-15 02:46:03 +0100

    Second commit

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
   mobile_menu.classList.toggle("is-active");
 });
+
+const tablinks = document.getElementsByClassName('tab-links');
+const tabs = document.getElementsByClassName('tab');
+
+const opentab = tabname => {
+    for(tablink of tablinks){
+      tablink.classList.remove("active-link");
+    }
+    for(tab of tabs){
+      tab.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
